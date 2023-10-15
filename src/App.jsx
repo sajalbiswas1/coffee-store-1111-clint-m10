59832c1faed3d060824225e2bc37a02d55c4f6fa
Programing Hero <sajalb1899@gmail.com>
2023-10-15 20:56:37 +0600

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-5xl text-purple-500'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h3>Add a coffe</h3>
+        </div>
+    );
+};
+
+export default AddCoffee;
